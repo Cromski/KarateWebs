d60bb0b64a8fb05daccf5d7c413b0165c3ccce0a
Cromski <jakobrhenriksen@outlook.com>
2023-07-16 23:56:08 +0200

    collage done on mobile

---
+++ src/components/Collage.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    //get list of images from blob
+    import FaEllipsisH from 'svelte-icons/fa/FaEllipsisH.svelte'
+    
+    // give me a list of 3 random numbers from 1-8
+    const randomNumbers = [1,2,3,4,5,6,7,8].sort(() => Math.random() - Math.random()).slice(0, 3);
+
+
+</script>
+
+<div class=" w-[90%] aspect-square mx-auto flex relative select-none cursor-pointer">
+   <img class=" object-cover w-1/3" src={`/blob/nature${randomNumbers[0]}.jpeg`} alt="">
+   <img class=" object-cover w-1/3" src={`/blob/nature${randomNumbers[1]}.jpeg`} alt="">
+   <img class=" object-cover w-1/3 brightness-50 " src={`/blob/nature${randomNumbers[2]}.jpeg`} alt="">
+   <h1 class=" absolute text-textColor right-[5%] text-[8vw] bottom-[30%]">Galleri</h1>
+   <div class=" absolute text-textColor w-[10%] right-[12%] bottom-[20%] ">
+       <FaEllipsisH />
+   </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
     import tilt from 'svelte-tilt'
+    import Collage from '../components/Collage.svelte';
+    import HexagonStack from '../components/HexagonStack.svelte';
+
 </script>
 
 <!-- https://github.com/LeoDog896/svelte-tilt -->
 <img  use:tilt={{ reverse: true}} src="/yellow.png" alt="" class="w-4/5 mx-auto mt-9">
 
-<h1 class=" text-center mx-3 mt-4 text-textColor">Cras sollicidtuion sodales quam, et tempus est bibendum eget. pPrawesent et velit ague. Quisque vulputate lettus sit amet scaliseique piacerat.</h1>
+<h1 class=" text-center mx-3 my-7 text-textColor">Cras sollicidtuion sodalfes quam, et tempus est bibendum eget. pPrawesent et velit ague. Quisque vulputate lettus sit amet scaliseique piacerat.</h1>
+
+<Collage />
+
+<HexagonStack />
+
+
+

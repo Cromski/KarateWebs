d47328d7c8588538a14613c19149d7a014b10d9b
Jakob Rasmus Henriksen <jarh@itu.dk>
2023-09-02 17:40:39 +0200

    more responsiveness

--- src/components/HexagonStack.svelte
+++ src/components/HexagonStack.svelte
@@ -4,9 +4,9 @@
   
 </script>
 
-<div class=" w-[320px] grid grid-cols-2 mt-20 [&>*:nth-child(3)]:col-span-2 sm:[&>*:nth-child(3)]:col-span-1 ">
+<div class="  w-[320px] grid grid-cols-2 [&>*:nth-child(3)]:col-span-2 lg:[&>*:nth-child(3)]:col-span-1 "> <!-- was sm -->
 
-    <MediaQuery query='(min-width: 641px)' let:matches>
+    <MediaQuery query='(min-width: 1024px)' let:matches> <!-- was 641 -->
         {#if matches}
         <Hexagon right={160} mt={0} text="Aktiviteter" imgPath="/karate.jpg" value={1} />
         <Hexagon right={160} mt={0} text="Aktiviteter" imgPath="/karate.jpg" value={2} />
@@ -17,7 +17,7 @@
         {/if}
     </MediaQuery>
 
-    <MediaQuery query='(max-width: 640px)' let:matches>
+    <MediaQuery query='(max-width: 1023px)' let:matches> <!-- was 640 -->
         {#if matches}
         <Hexagon right={0} mt={0} text="Aktiviteter" imgPath="/karate.jpg" value={1} />
         <Hexagon right={0} mt={0} text="Aktiviteter" imgPath="/karate.jpg" value={2} />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,18 +5,20 @@
     import Nyheder from '../components/Nyheder.svelte';
 </script>
 
-<div class=" grid grid-cols-1 sm:grid-cols-2 gap-11">
+<div class=" grid grid-cols-1 sm:grid-cols-2 gap-2">
     <div>
         <!-- https://github.com/LeoDog896/svelte-tilt -->
         <img  use:tilt={{ reverse: true}} src="/bsi-logo.png" alt="" class="w-4/5 mx-auto mt-14 max-w-[60%]">
         <h1 class=" text-center mx-auto my-7 text-textColor text-xl w-4/5 ">Cras sollicidtuion sodalfes quam, et tempus est bibendum eget. pPrawesent et velit ague. Quisque vulputate lettus sit amet scaliseique piacerat.</h1>
     </div>
     
-    <Collage />
+    <!-- <Collage /> -->
     
     <Nyheder />
-    
-    <div class=" mx-auto sm:mx-0">
+    <Nyheder />
+
+
+    <div class=" mt-0 sm:-mt-[65px] lg:mt-[10%] scale-100 sm:scale-75 lg:scale-100 mx-auto lg:mx-0 xl:ml-[15%]  ">
         <HexagonStack />
     </div>
     

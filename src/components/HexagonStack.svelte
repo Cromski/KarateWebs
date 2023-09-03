639b4ec0c8cbca88b6971cfda57f5474c313fa38
Jakob Rasmus Henriksen <jarh@itu.dk>
2023-09-03 22:40:41 +0200

    created links in hexagons

--- src/components/HexagonStack.svelte
+++ src/components/HexagonStack.svelte
@@ -8,23 +8,23 @@
 
     <MediaQuery query='(min-width: 1024px)' let:matches> <!-- was 641 -->
         {#if matches}
-        <Hexagon right={160} mt={0} text="Aktiviteter" imgPath="/karates.jpg" value={1} />
-        <Hexagon right={160} mt={0} text="Aktiviteter" imgPath="/karates.jpg" value={2} />
-        <Hexagon right={80} mt={24} text="Aktiviteter" imgPath="/karates.jpg" value={3} />
-        <Hexagon right={80} mt={24} text="Aktiviteter" imgPath="/karates.jpg" value={4} />
-        <Hexagon right={0} mt={24} text="Aktiviteter" imgPath="/karates.jpg"  value={5} />
-        <Hexagon right={0} mt={24} text="Aktiviteter" imgPath="/karates.jpg"  value={6} />
+        <Hexagon right={160} mt={0} text="Aktiviteter" imgPath="/karates.jpg" path={"/aktiviteter"} />
+        <Hexagon right={160} mt={0} text="Om karate" imgPath="/karates.jpg" path={"/om-karate"} />
+        <Hexagon right={80} mt={24} text="Kontakt os" imgPath="/karates.jpg" path={"/kontakt-os"} />
+        <Hexagon right={80} mt={24} text="Nyheder" imgPath="/karates.jpg" path={"/nyheder"} />
+        <Hexagon right={0} mt={24} text="Bliv medlem" imgPath="/karates.jpg"  path={"/bliv-medlem"} />
+        <Hexagon right={0} mt={24} text="Galleri" imgPath="/karates.jpg"  path={"/galleri"} />
         {/if}
     </MediaQuery>
 
     <MediaQuery query='(max-width: 1023px)' let:matches> <!-- was 640 -->
         {#if matches}
-        <Hexagon right={0} mt={0} text="Aktiviteter" imgPath="/karates.jpg" value={1} />
-        <Hexagon right={0} mt={0} text="Aktiviteter" imgPath="/karates.jpg" value={2} />
-        <Hexagon right={80} mt={24} text="Aktiviteter" imgPath="/karates.jpg" value={3} />
-        <Hexagon right={0} mt={24} text="Aktiviteter" imgPath="/karates.jpg" value={4} />
-        <Hexagon right={0} mt={24} text="Aktiviteter" imgPath="/karates.jpg"  value={5} />
-        <Hexagon right={80} mt={24} text="Aktiviteter" imgPath="/karates.jpg"  value={6} />
+        <Hexagon right={0} mt={0} text="Aktiviteter" imgPath="/karates.jpg" path={"/aktiviteter"} />
+        <Hexagon right={0} mt={0} text="Om karate" imgPath="/karates.jpg" path={"/om-karate"} />
+        <Hexagon right={80} mt={24} text="Kontakt os" imgPath="/karates.jpg" path={"/kontakt-os"} />
+        <Hexagon right={0} mt={24} text="Nyheder" imgPath="/karates.jpg" path={"/nyheder"} />
+        <Hexagon right={0} mt={24} text="Bliv medlem" imgPath="/karates.jpg"  path={"/bliv-medlem"} />
+        <Hexagon right={80} mt={24} text="Galleri" imgPath="/karates.jpg"  path={"/galleri"} />
         {/if}
     </MediaQuery>
 
--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -2,9 +2,11 @@
     import MdMenu from 'svelte-icons/md/MdMenu.svelte'
 </script>
 
-<div class="w-full h-24 bg-backgroundLighter flex justify-between">
-    <img src="/bsi-logo.png" alt="" class=" ml-10 h-4/5 inline-block my-auto">
-    <button class="inline-block scale-50 md:hidden ">
+<div class="w-full h-24 bg-backgroundLighter flex justify-between relative">
+    <a class=" flex" href="/">
+        <img src="/bsi-logo.png" alt="" class=" ml-10 h-4/5 inline-block my-auto">
+    </a>
+    <button  class="inline-block scale-50 md:hidden ">
         <MdMenu />
     </button>
     <div class="hidden text-textColor items-center md:flex gap-6 lg:gap-12 text-lg lg:text-xl mr-4 lg:mr-8 2xl:text-2xl 2xl:gap-20 2xl:mr-16">

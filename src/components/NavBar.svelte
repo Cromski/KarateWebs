aab52ee40b3896b49e2d97dd68e5813495a15309
Jakob Rasmus Henriksen <jarh@itu.dk>
2023-09-03 15:58:31 +0200

    Update NavBar.svelte

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -3,8 +3,16 @@
 </script>
 
 <div class="w-full h-24 bg-backgroundLighter flex justify-between">
-    <img src="/yellow.png" alt="" class=" ml-10 h-4/5 inline-block my-auto">
-    <button class="inline-block scale-50">
+    <img src="/bsi-logo.png" alt="" class=" ml-10 h-4/5 inline-block my-auto">
+    <button class="inline-block scale-50 lg:hidden ">
         <MdMenu />
     </button>
+    <div class="hidden lg:flex justify-between gap-28 text-2xl mr-20">
+        <button>Aktiviteter</button>
+        <button>Om karate</button>
+        <button>Kontakt os</button>
+        <button>Nyheder</button>
+        <button>Bliv medlem</button>
+        <button>Galleri</button>
+    </div>
 </div>

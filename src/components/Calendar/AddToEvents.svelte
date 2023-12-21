bd4305cf97f8b85194b215433c9b51ff416a0ac7
Cromski <jakobrhenriksen@outlook.com>
2023-12-21 16:50:09 +0100

    todo: add event to storage i guess

---
+++ src/components/Calendar/AddToEvents.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+
+    let today = new Date()
+    let tomorrow = new Date(today.getTime() + (24 * 60 * 60 * 1000))
+    const formatDate = (date: Date) => `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`
+
+    let title: string = "sd"
+    let date1: string
+    let date2: string
+
+    $: today, date1 = formatDate(today)
+    $: tomorrow, date2 = formatDate(tomorrow)
+
+</script>
+
+<div >
+    <input type="text" bind:value={title} />
+    <input type="date" bind:value={date1} />
+    <input type="date" bind:value={date2} />
+    <button class=" bg-textColor" on:click={() => console.log(title, date1, date2)}>Add</button>
+</div>
--- src/routes/aktiviteter/+page.svelte
+++ src/routes/aktiviteter/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import FullCalendar from "../../components/Calendar/FullCalendar.svelte";
+    import AddToEvents from "../../components/Calendar/AddToEvents.svelte";
 </script>
 
+<AddToEvents />
 <FullCalendar />

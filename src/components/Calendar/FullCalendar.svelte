afc3348d697dd9272bc0a90657c8e43b3b9792a0
Jakob Rasmus Henriksen <jarh@itu.dk>
2023-12-27 13:51:18 +0100

    responsive now?

--- src/components/Calendar/FullCalendar.svelte
+++ src/components/Calendar/FullCalendar.svelte
@@ -7,6 +7,7 @@
     import monthsStore from "../../stores/calendar/MonthsStore";
     import FullDayModal from "./FullDayModal.svelte";
     import eventsStore from "../../stores/calendar/EventStore";
+    import MediaQuery from 'svelte-media-queries'
     
     const daysInMonth = [31, new Date().getFullYear() == 366 ? 29 : 28,31,30,31,30,31,31,30,31,30,31];
 
@@ -48,12 +49,31 @@
     <button on:click={() => updateViewSize(viewSize.value)}>{viewSize.name}</button>
 {/each}
 
-<div style={`width: ${viewSize}%`} class=" relative mx-auto flex justify-center">
-    {#each $selectedMonthsStore as month}
-        <Month month={month} daysInMonth={daysInMonth[month]} />
-    {/each}
-    <FullDayModal />
-</div>
+<MediaQuery query='(max-width: 767px)' let:matches> <!-- was 641 -->
+    {#if matches}
+        <div class=" w-full [overflow:overlay] ">
+            <div style={`width: 800px`} class=" relative mx-auto flex justify-center">
+                {#each $selectedMonthsStore as month}
+                    <Month month={month} daysInMonth={daysInMonth[month]} />
+                {/each}
+                <FullDayModal />
+            </div>
+        </div>
+    {/if}
+</MediaQuery>
+
+<MediaQuery query='(min-width: 768px)' let:matches> <!-- was 641 -->
+    {#if matches}
+        <div style={`width: ${viewSize}%`} class=" relative mx-auto flex justify-center">
+            {#each $selectedMonthsStore as month}
+                <Month month={month} daysInMonth={daysInMonth[month]} />
+            {/each}
+            <FullDayModal />
+        </div>
+    {/if}
+</MediaQuery>
+
+
 
 
 <!-- <h1>{selectedMonths}</h1> -->

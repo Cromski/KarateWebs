4209f62e61fc4f94a5921cb29ebc09785ac46fd0
Jakob Rasmus Henriksen <jarh@itu.dk>
2023-12-31 16:15:19 +0100

    fix route in hexagons

--- src/components/HexagonStack.svelte
+++ src/components/HexagonStack.svelte
@@ -10,7 +10,7 @@
         {#if matches}
         <Hexagon right={160} mt={0} text="Aktiviteter" imgPath="/karates.jpg" path={"/aktiviteter"} bgSize={300} x={-3} y={0} />
         <Hexagon right={160} mt={0} text="Om karate" imgPath="/karate-girl.jpg" path={"/om-karate"} bgSize={350} x={-6.5} y={-1} />
-        <Hexagon right={80} mt={24} text="Kontakt os" imgPath="/282_282.jpg" path={"/kontakt-os"} bgSize={250} x={-2.5} y={0} />
+        <Hexagon right={80} mt={24} text="Kontakt os" imgPath="/282_282.jpg" path={"/kontakt"} bgSize={250} x={-2.5} y={0} />
         <Hexagon right={80} mt={24} text="Nyheder" imgPath="/bearded-karate-fighter.jpg" path={"/nyheder"} bgSize={300} x={0} y={-1} />
         <Hexagon right={0} mt={24} text="Bliv medlem" imgPath="/young-man-are-training.jpg"  path={"/bliv-medlem"} bgSize={250} x={-6} y={0} />
         <Hexagon right={0} mt={24} text="Galleri" imgPath="/shaved-head-karate.jpg"  path={"/galleri"} bgSize={250} x={-2} y={0} />
@@ -21,7 +21,7 @@
         {#if matches}
         <Hexagon right={0} mt={0} text="Aktiviteter" imgPath="/karates.jpg" path={"/aktiviteter"} bgSize={300} x={-3} y={0} />
         <Hexagon right={0} mt={0} text="Om karate" imgPath="/karate-girl.jpg" path={"/om-karate"} bgSize={350} x={-6.5} y={-1} />
-        <Hexagon right={80} mt={24} text="Kontakt os" imgPath="/282_282.jpg" path={"/kontakt-os"} bgSize={250} x={-2.5} y={0} />
+        <Hexagon right={80} mt={24} text="Kontakt os" imgPath="/282_282.jpg" path={"/kontakt"} bgSize={250} x={-2.5} y={0} />
         <Hexagon right={0} mt={24} text="Nyheder" imgPath="/bearded-karate-fighter.jpg" path={"/nyheder"} bgSize={300} x={0} y={-1} />
         <Hexagon right={0} mt={24} text="Bliv medlem" imgPath="/young-man-are-training.jpg"  path={"/bliv-medlem"} bgSize={250} x={-6} y={0} />
         <Hexagon right={80} mt={24} text="Galleri" imgPath="/shaved-head-karate.jpg"  path={"/galleri"} bgSize={250} x={-2} y={0} />

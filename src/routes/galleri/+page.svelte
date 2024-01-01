83ed590915b7be9320eb75939ed0b96d7747edb1
Jakob Rasmus Henriksen <jarh@itu.dk>
2024-01-01 17:03:53 +0100

    created /galleri

---
+++ src/routes/galleri/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    let files;
+
+    $: console.log(files)
+
+</script>
+
+<div class=" flex justify-center mt-8">
+    <input type="file" bind:files multiple class=" file-input file-input-bordered w-full max-w-sm" />
+</div>
+
+{#if files}
+    {#each Array.from(files) as file }
+        <h1>{file.name}</h1>
+    {/each}
+{/if}

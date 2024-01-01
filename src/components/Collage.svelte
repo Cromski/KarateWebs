dcad68ca88fc5a2805835cda1c45ea70bae89edf
Jakob Rasmus Henriksen <jarh@itu.dk>
2024-01-01 18:45:04 +0100

    galleri collage now navigates to /galleri

--- src/components/Collage.svelte
+++ src/components/Collage.svelte
@@ -8,14 +8,14 @@
 
 </script>
 
-<div class=" w-[90%] aspect-square mx-auto flex relative select-none cursor-pointer
-             sm:w-[70%] sm:my-auto ">
-   <img class=" object-cover w-1/3" src={`/blob/nature${randomNumbers[0]}s.jpg`} alt="">
-   <img class=" object-cover w-1/3" src={`/blob/nature${randomNumbers[1]}s.jpg`} alt="">
-   <img class=" object-cover w-1/3 brightness-50 " src={`/blob/nature${randomNumbers[2]}s.jpg`} alt="">
-   <h1 class=" absolute text-textColor right-[5%] text-[8vw] bottom-[30%]
-               sm:text-[3vw]">Galleri</h1>
-   <div class=" absolute text-textColor w-[10%] right-[12%] bottom-[20%] ">
-       <FaEllipsisH />
-   </div>
-</div>
+<a href="/galleri" class=" w-[90%] aspect-square mx-auto flex relative select-none cursor-pointer
+sm:w-[70%] sm:my-auto ">
+        <img class=" object-cover w-1/3" src={`/blob/nature${randomNumbers[0]}s.jpg`} alt="">
+        <img class=" object-cover w-1/3" src={`/blob/nature${randomNumbers[1]}s.jpg`} alt="">
+        <img class=" object-cover w-1/3 brightness-50 " src={`/blob/nature${randomNumbers[2]}s.jpg`} alt="">
+        <h1 class=" absolute text-textColor right-[5%] text-[8vw] bottom-[30%]
+                    sm:text-[3vw]">Galleri</h1>
+        <div class=" absolute text-textColor w-[10%] right-[12%] bottom-[20%] ">
+            <FaEllipsisH />
+    </div>
+</a>

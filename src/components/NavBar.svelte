da2426c8ca1dadc5699a26f55dd2fe0d19037699
Jakob Rasmus Henriksen <jarh@itu.dk>
2024-01-01 19:44:18 +0100

    added navbar menu

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,20 +1,40 @@
 <script lang="ts">
     import MdMenu from 'svelte-icons/md/MdMenu.svelte'
+    import NavBarOpen from '../stores/NavMenuOpenStore';
+
+    $: console.log($NavBarOpen)
+
+    const navBarItems = [
+        {text: "Aktiviteter", route: "/aktiviteter"},
+        {text: "Om karate", route: "/om-karate"},
+        {text: "Kontakt os", route: "/kontakt"},
+        {text: "Nyheder", route: "/nyheder"},
+        {text: "Bliv medlem", route: "/bliv-medlem"},
+        {text: "Galleri", route: "/galleri"},
+    ]
+
 </script>
 
 <div class="w-full h-24 bg-backgroundLighter flex justify-between relative">
     <a class=" flex" href="/">
         <img src="/bsi-logo.png" alt="" class=" ml-10 h-4/5 inline-block my-auto">
     </a>
-    <button  class="inline-block scale-50 md:hidden ">
+    <!-- small screen -->
+    <button class=" md:hidden inline-block scale-50 " on:click={() => NavBarOpen.set(!$NavBarOpen)}>
         <MdMenu />
     </button>
+    {#if $NavBarOpen}
+        <div class="absolute top-[98px] md:hidden z-10 bg-backgroundLighter w-full grid grid-cols-1 ">
+            {#each navBarItems as item}
+                <a class=" text-right text-xl font-semibold pr-14 h-14 leading-[56px] hover:bg-backgroundColor" href={item.route}>{item.text}</a>
+            {/each}
+        </div>
+    {/if}
+    
+    <!--  big screen  -->
     <div class="hidden text-textColor items-center md:flex gap-6 lg:gap-12 text-lg lg:text-xl mr-4 lg:mr-8 2xl:text-2xl 2xl:gap-20 2xl:mr-16">
-        <a href="/aktiviteter">Aktiviteter</a>
-        <a href="/om-karate">Om karate</a>
-        <a href="/kontakt">Kontakt os</a>
-        <a href="/nyheder">Nyheder</a>
-        <a href="/bliv-medlem">Bliv medlem</a>
-        <a href="/galleri">Galleri</a>
+        {#each navBarItems as item}
+            <a href={item.route}>{item.text}</a>
+        {/each}
     </div>
 </div>
---
+++ src/stores/NavMenuOpenStore.ts
@@ -0,0 +1,5 @@
+import { writable } from "svelte/store";
+
+const NavBarOpen = writable(true);
+
+export default NavBarOpen;
